bc7a9e748c69833b15dbee0a96ecd1c828751b5b
i.bobrov <i.bobrov@itfbgroup.ru>
2024-11-04 07:34:36 +1000

    upd: уход в rest

--- src/main/kotlin/ru/bobrov/clouddrive/controllers/FoldersController.kt
+++ src/main/kotlin/ru/bobrov/clouddrive/controllers/FoldersController.kt
@@ -1,31 +1,29 @@
 package ru.bobrov.clouddrive.controllers
 
 import org.springframework.data.domain.Sort
-import org.springframework.stereotype.Controller
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import ru.bobrov.clouddrive.dto.FolderCreateResponse
 import ru.bobrov.clouddrive.model.Directory
 import ru.bobrov.clouddrive.repositories.DirectoryRepository
-import java.util.*
 
-@Controller
-class IndexController(
+@RestController("/api/folders")
+class FoldersController(
     val directoryRepository: DirectoryRepository
 ) {
 
-    @GetMapping("/", "/index")
-    fun getIndexPage(model: Model) : String {
-        model.addAttribute("directories", directoryRepository.findAll(Sort.by(Sort.Order.asc("name"))))
-        return "index"
-    }
+    @GetMapping
+    fun findAll(model: Model): Iterable<Directory> =
+        directoryRepository.findAll(Sort.by(Sort.Order.asc("name")))
 
-    @PostMapping("/api/folders")
-    fun createDirectory(@RequestBody response: FolderCreateResponse) : String {
+    @PostMapping
+    fun save(@RequestBody response: FolderCreateResponse): ResponseEntity<Void> {
         directoryRepository.save(Directory(null, response.name))
-
-        return "redirect:/"
+        return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 }
